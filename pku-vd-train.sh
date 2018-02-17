08ed4f27512d4a8a2bb3e2618c7bd249ce06ecb6
knwng <ken.qrose@gmail.com>
2018-02-18 00:02:22 +0800

    trainable spatial attention code

--- pku-vd-train.sh
+++ pku-vd-train.sh
@@ -3,27 +3,21 @@
 # This file calls train.py with all hyperparameters as for the TriNet
 # experiment on market1501 in the original paper.
 
-# if [ "$#" -lt 3 ]; then
-#     echo "Usage: $0 PATH_TO_IMAGES RESNET_CHECKPOINT_FILE EXPERIMENT_ROOT ..."
-#     echo "See the README for more info"
-#     echo "Download ResNet-50 checkpoint from https://github.com/tensorflow/models/tree/master/research/slim#pre-trained-models"
-#     exit 1
-# fi
-
 # Shift the arguments so that we can just forward the remainder.
-# IMAGE_ROOT=$1 ; shift
-# INIT_CHECKPT=$1 ; shift
-# EXP_ROOT=$1 ; shift
+export CUDA_VISIBLE_DEVICES=1
+
+METRIC='euclidean'
+HEADS='fc1024_spatial_attention'
+BACKBONE='resnet_v2_50'
+EXPR_NAME='_0'
 
 IMAGE_ROOT=/home/qwang/Dataset/PKU-VD/VD1/ ; shift
-# INIT_CHECKPT=./pretrained_models/resnet_v1_101.ckpt ; shift
-INIT_CHECKPT=./pretrained_models/inception_v4.ckpt ; shift
-EXP_ROOT=./experiments/pku-vd/expr_inception_v4 ; shift
-
+INIT_CHECKPT=./pretrained_models/resnet_v2_50.ckpt ; shift
+EXP_ROOT=./experiments/pku-vd/expr_attention_${METRIC}_${HEADS}_${BACKBONE}${EXPR_NAME} ; shift
 
 python train.py \
     --train_set data/pku-vd/VD1_train.csv \
-    --model_name inception \
+    --model_name ${BACKBONE} \
     --image_root $IMAGE_ROOT \
     --initial_checkpoint $INIT_CHECKPT \
     --experiment_root $EXP_ROOT \
@@ -36,14 +30,14 @@ python train.py \
     --pre_crop_height 300 --pre_crop_width 300 \
     --net_input_height 224 --net_input_width 224 \
     --margin soft \
-    --metric euclidean \
+    --metric ${METRIC} \
     --loss batch_hard \
+    --head_name ${HEADS} \
     --learning_rate 1e-4 \
     --train_iterations 400000 \
-    --decay_start_iteration 10000 \
+    --decay_start_iteration 0 \
     --lr_decay_factor 0.96 \
     --lr_decay_steps 4000 \
     --weight_decay_factor 0.0002 \
-    --detailed_logs \
     "$@"
     # --resume \
