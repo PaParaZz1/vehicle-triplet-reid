fix false formula

RL_utils.py
@@ -32,6 +32,7 @@ class PolicyGradient:
             output_graph=False,
             is_train=True,
             rl_activation='softmax',
+            rl_hidden_units=256,
             ):
 
         self.n_actions = n_actions
@@ -40,6 +41,7 @@ class PolicyGradient:
         self.gamma = reward_decay
         self.is_train = is_train
         self.rl_activation = rl_activation
+        self.rl_hidden_units = rl_hidden_units
 
         self.ep_obs, self.ep_as, self.ep_rs = None, None, None
 
@@ -74,7 +76,7 @@ class PolicyGradient:
                         self.tf_obs = tf.placeholder(tf.float32, [None, self.n_features], name="rl_observations")
                         self.tf_acts = tf.placeholder(tf.float32, [None, self.n_features], name="rl_actions_num")
                         self.tf_vt = tf.placeholder(tf.float32, [None, ], name="rl_actions_value")
-                        dense1 = slim.fully_connected(self.tf_obs, 1024, scope='rl_dense1')
+                        dense1 = slim.fully_connected(self.tf_obs, self.rl_hidden_units, scope='rl_dense1')
                         dense2 = slim.fully_connected(dense1, self.n_actions, scope='rl_dense2')
 
                         if self.rl_activation == 'softmax':
@@ -122,11 +124,11 @@ class PolicyGradient:
                 action.append([np.random.choice([0, 1], p=[x, 1-x]) for x in batch])
         else:
             # prob_weights = [(x - np.min(x)) / (np.max(x) - np.min(x) + 1e-5) for x in prob_weights]
-            # action = np.around(prob_weights)
+            action = np.around(prob_weights)
             # action = np.where(prob_weights > 0.75, 
             #         np.ones_like(prob_weights), np.zeros_like(prob_weights))
             # print('action: {}'.format(np.mean(np.count_nonzero(action, axis=1))))
-            action = prob_weights
+            # action = prob_weights
         return action
 
     def store_transition(self, s, a, r):

train_rl_parallel.py
@@ -502,7 +502,7 @@ def main():
                 
                 # normalize reward
                 rl_rewards -= np.mean(rl_rewards, axis=0)
-                rl_rewards /= np.std(rl_rewards, axis=0)
+                # rl_rewards /= np.std(rl_rewards, axis=0)
 
                 Agent.store_transition(b_ftrs_batch, rl_actions, rl_rewards)
                 rl_losses = Agent.learn()
@@ -510,42 +510,6 @@ def main():
                 # step = sess_rl.run(Agent.global_step)
                 elapsed_time = time.time() - start_time
                 log.info('RL | Step {} | Action {:.2f} | Reward {: .4e} | Loss {: .4e} | Speed {:.2f}s/iter'.format(step, np.mean(np.count_nonzero(rl_actions, axis=1)), np.mean(rl_rewards), np.mean(rl_losses), elapsed_time))
-
-                '''
-                # Supervised Learning
-                sup_iter = tf.data.Dataset.from_tensor_slices((b_imgs, b_fids, b_pids)).batch(args.batch_p * args.batch_k).prefetch(args.batch_p * args.batch_k).make_one_shot_iterator()
-                sup_handle = sess.run(sup_iter.string_handle())
-                _, summary, step, b_prec_at_k, b_embs, b_loss, b_fids, \
-                _pos_dist, _neg_dist, _pos_indices, _neg_indices = \
-                    sess.run([train_op, merged_summary, global_step, \
-                    prec_at_k, endpoints['emb'], losses, fids, \
-                    pos_dists, neg_dists, pos_indices, neg_indices], feed_dict={handle:sup_handle})
-                elapsed_time = time.time() - start_time
-
-                # Compute the iteration speed and add it to the summary.
-                # We did observe some weird spikes that we couldn't track down.
-                summary2 = tf.Summary()
-                summary2.value.add(tag='secs_per_iter', simple_value=elapsed_time)
-                summary_writer.add_summary(summary2, step)
-                summary_writer.add_summary(summary, step)
-
-                if args.detailed_logs:
-                    log_embs[i], log_loss[i], log_fids[i] = b_embs, b_loss, b_fids
-
-                # Do a huge print out of the current progress.
-                seconds_todo = (args.train_iterations - step) * elapsed_time
-                log.info('iter:{:6d}, loss min|avg|max: {:.3f}|{:.3f}|{:6.3f}, '
-                         'batch-p@{}: {:.2%}, ETA: {} ({:.2f}s/it)'.format(
-                             step,
-                             float(np.min(b_loss)),
-                             float(np.mean(b_loss)),
-                             float(np.max(b_loss)),
-                             args.batch_k-1, float(b_prec_at_k),
-                             timedelta(seconds=int(seconds_todo)),
-                             elapsed_time))
-                sys.stdout.flush()
-                sys.stderr.flush()
-                '''
 
                 # Save a checkpoint of training every so often.
                 if (args.checkpoint_frequency > 0 and step % args.checkpoint_frequency == 0):