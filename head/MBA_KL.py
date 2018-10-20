dca58e3092e4667a2c3b9952b505c179a80eff27
niuyazhe <niuyazhe@buaa.edu.cn>
2018-10-20 17:00:10 +0800

    test attention module

--- head/MBA_KL.py
+++ head/MBA_KL.py
@@ -1,34 +1,35 @@
 import numpy as np
 import torch
 import torch.nn as nn
+from torch.autograd import Variable
 
 from modules import *
 
 class AttentionModule(nn.Module):
     def __init__(self, input_dim):
         super(AttentionModule, self).__init__()
-        self.conv1 = ConvBlockSequentail(in_channels = input_dim, out_channels = 64, kernel_size = 1, init_type = "xavier", activation = nn.ReLU(), use_batchnorm = True)
-        self.conv2 = ConvBlockSequentail(in_channels = 64, out_channels = 1, kernel_size = 1, init_type = "xavier", activation = None, use_batchnorm = True)
+        self.conv1 = ConvBlockSequential(in_channels = input_dim, out_channels = 64, kernel_size = 1, init_type = "xavier", activation = nn.ReLU(), use_batchnorm = True)
+        self.conv2 = ConvBlockSequential(in_channels = 64, out_channels = 1, kernel_size = 1, init_type = "xavier", activation = None, use_batchnorm = True)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         b, c, h, w = x.shape
         feature = x
-        if h != w
+        if h != w:
             raise BaseException("input feature h not match w\n")
         mask = self.conv1(x)
         mask = self.conv2(mask)
         mask = self.sigmoid(mask)
-        identity = torch.from_numpy(np.eye(h).astype(np.float32)).repeat(b)
+        identity = Variable(torch.from_numpy(np.eye(h).astype(np.float32)).repeat(b, 1, 1, 1)).cuda()
         mask = mask + identity
-        mask_map = torch.zeros(b, c, h, w)
-        torch.addcmul(mask_map, 1, mask, feature)
+        mask = mask.repeat(1,c,1,1)
+        mask_map = mask * feature
         return mask_map
 
 if __name__ == "__main__":
-    from torch.autograd import Variable
+    # attention module test
     inputs = torch.rand(8,128,224,224).cuda()
     inputs = Variable(inputs)
     model = AttentionModule(128).cuda()
     out = model(inputs)
-    print("end")
+    print("attention module test pass")
